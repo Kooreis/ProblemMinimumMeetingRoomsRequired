9d53c1bc64ee9501d1bc52d054c0c538e9478275
Kooreis <kooreis.official@gmail.com>
2024-12-16 09:54:05 -0600

    This part of the script imports the heapq module and defines the function min_meeting_rooms. This function sorts the intervals by start time and initializes a heap with the end time of the first interval.

--- solution.py
+++ solution.py
@@ -1,6 +1,3 @@
-Here is a Python console application that solves the problem:
-
-```python
 import heapq
 
 def min_meeting_rooms(intervals):
@@ -11,26 +8,3 @@ def min_meeting_rooms(intervals):
 
     meeting_rooms = []
     heapq.heappush(meeting_rooms, intervals[0][1])
-
-    for i in range(1, len(intervals)):
-        if meeting_rooms[0] <= intervals[i][0]:
-            heapq.heappop(meeting_rooms)
-
-        heapq.heappush(meeting_rooms, intervals[i][1])
-
-    return len(meeting_rooms)
-
-def main():
-    intervals = []
-    n = int(input("Enter the number of meetings: "))
-    for i in range(n):
-        start_time = int(input("Enter the start time of meeting {}: ".format(i+1)))
-        end_time = int(input("Enter the end time of meeting {}: ".format(i+1)))
-        intervals.append((start_time, end_time))
-    print("Minimum number of meeting rooms required: ", min_meeting_rooms(intervals))
-
-if __name__ == "__main__":
-    main()
-```
-
-This console application first asks for the number of meetings. Then for each meeting, it asks for the start and end times. It then calculates and prints the minimum number of meeting rooms required. The calculation is done by sorting the intervals by start time, then using a priority queue (implemented with a heap) to keep track of end times. If the start time of the next meeting is after the end time of the earliest ending meeting, we can use the same room, so we pop from the heap. Otherwise, we need a new room, so we push onto the heap. The size of the heap at the end is the minimum number of rooms required.
