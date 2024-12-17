2dd80f322fa49c43786c05a9627d43f9cf9a3bfa
Kooreis <kooreis.official@gmail.com>
2024-12-17 08:54:05 -0600

    Defining the main class and the main method. The main method initializes an array of intervals and calls the minMeetingRooms function with this array as an argument.

--- solution.java
+++ solution.java
@@ -1,2 +1,5 @@
-import java.util.Arrays;
-import java.util.PriorityQueue;
+public class Main {
+    public static void main(String[] args) {
+        int[][] intervals = {{0, 30}, {5, 10}, {15, 20}};
+        System.out.println(minMeetingRooms(intervals));
+    }
